c3ed78ec56fd381bc2eb37eb0e5315d0e9a80c13
lo <lo@ldxbubble.2>
2008-09-22 22:13:38 +0800

    Added unit tests and cleanup for check_proxy, better return values for each function

--- scapegoat.py
+++ scapegoat.py
@@ -6,74 +6,63 @@ from urllib2 import urlopen, build_opener
 from urllib2 import HTTPHandler, ProxyHandler, HTTPError, URLError
 from optparse import OptionParser
 
-# IP regex 
-r_ip = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-# nmap grepable format : port/state, check for port/open
-r_open_port = re.compile('\d{1,5}/open')
-
-g_control_data = ''
-
 socket.setdefaulttimeout(10)
 
-TEMP_LOG = '.log.tmp'
-CONTROL_URL = 'http://chioka.in/wp-content/themes/girl/style.css'
-
 def run_nmap(args):
-    global TEMP_LOG
 
     cmd = 'nmap'
     cmd += ' -q'
-    cmd += ' -oG %s' % (TEMP_LOG)
+    cmd += ' -oG %s' % (args['output'])
     cmd += ' -p %s' % (args['-p'])
     cmd += ' %s' % (args['ip'])
 
     if args['verbose'] == False:
         cmd += ' %s' % ('1>/dev/null') # send all the output to hell
 
-    print cmd
+    #print cmd
     os.system(cmd)
-    pass
 
-def check_proxys():
-    global r_ip, r_open_port
-    global g_control_data
-    global CONTROL_URL
+def check_proxys(test_url, nmap_log_file):
+
+    # IP regex 
+    r_ip = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+    # nmap grepable format : port/state, check for port/open
+    r_open_port = re.compile('\d{1,5}/open')
+
+    proxys = []
 
     try:
-        url_sock = urlopen(CONTROL_URL)
-        g_control_data = url_sock.read()
+        url_sock = urlopen(test_url)
+        test_data = url_sock.read()
     except URLError, e:
         print 'Control : %s ' % (e)
-        return 
+        return proxys
     except HTTPError, e:
         print 'Control : %s ' % (e)
-        return
+        return proxys
     except:
         print 'Control : Other Error'
-        return
+        return proxys
 
-    log_data = [ i.rstrip() for i in open(TEMP_LOG, 'r').readlines() ]
+    log_data = [ i.rstrip() for i in open(nmap_log_file, 'r').readlines() ]
 
     for line in log_data:
-        #print 'Studying : [%s]' % ( line )
         if r_open_port.search(line):
             open_ports = r_open_port.findall(line)
-
             ip = r_ip.findall(line)[0]
-            print 'Inspecting %s' % (ip)
 
             for open_port in open_ports:
                 port = open_port.split('/')[0]
-                check_proxy(ip, port)
-
+                if check_proxy(ip, port, test_url, test_data):
+                    proxys.append( {'ip':ip, 'port':port, 'user':'', 'pass':''} )
 
     #delete log file!
-    pass
+    os.remove( nmap_log_file )
 
-def check_proxy(ip, port):
-    global g_control_data
-    global CONTROL_URL
-    print 'Checking %s:%s' % ( ip, port)
+    return proxys
+
+def check_proxy(ip, port, test_url, test_data):
+    #print 'Checking %s:%s' % ( ip, port)
 
     proxy_info = {
         'user' : '',
@@ -82,19 +71,13 @@ def check_proxy(ip, port):
         'port' : port,
     }
 
-    #proxy_dict = {'http':'%(user)s:%(pass)s@%(host)s:%(port)s' % (proxy_info)}
-    proxy_dict = {'http':'%(host)s:%(port)s' % (proxy_info)}
-    print proxy_dict
-    proxy_handler = ProxyHandler({'http':proxy_dict})
+    proxy_dict = {'http':'%(user)s:%(pass)s@%(host)s:%(port)s' % (proxy_info)}
+    proxy_handler = ProxyHandler(proxy_dict)
 
-    opener = build_opener(proxy_handler, HTTPHandler)
-    print 'URL OPener done'
-    url_sock = opener.open(CONTROL_URL)
-    print 'URL Opened'
     try:
+        opener = build_opener(proxy_handler, HTTPHandler)
+        url_sock = opener.open(test_url)
         url_data = url_sock.read()
-        print 'URL read data'
-
     except URLError, e:
         print e
         return
@@ -105,24 +88,26 @@ def check_proxy(ip, port):
         print 'Other Error '
         return
 
-    if url_data == g_control_data:
-        print 'Good Proxy : %(user)s:%(pass)s@%(host)s:%(port)s' % (proxy_info)
+    if url_data == test_data:
+        #print 'Good Proxy : %(user)s:%(pass)s@%(host)s:%(port)s' % (proxy_info)
+        return True
     else:
-        print '=========='
-        print url_data
-        print '=========='
-        print g_control_data
-        print '=========='
-        print 'Bad Proxy : %(user)s:%(pass)s@%(host)s:%(port)s' % (proxy_info)
-
-
+        #print '=========='
+        #print url_data
+        #print '=========='
+        #print test_data
+        #print '=========='
+        #print 'Bad Proxy : %(user)s:%(pass)s@%(host)s:%(port)s' % (proxy_info)
+        return False
 
 
 if __name__ == '__main__':
+
     optparser = OptionParser()
     optparser.add_option('-t', '--target', dest='target', help='Target range to scan, in nmap format. Default is Random Scan on nmap.', default=None)
     optparser.add_option('-p', '--port', dest='port', help='Port range to scan, in nmap format. Default is 80,3128,8080', default='80,3128,8080')
     optparser.add_option('-q', '--quiet', action='store_false', dest='verbose', help='No output from nmap.', default=True)
+    optparser.add_option('-u', '--url', dest='url', help='URL to validate proxy against, default is http://chioka.in/wp-content/themes/girl/style.css', default='http://chioka.in/wp-content/themes/girl/style.css')
 
     (op, args) = optparser.parse_args()
 
@@ -140,6 +125,7 @@ if __name__ == '__main__':
     nmap_args['ip'] = target
     nmap_args['-p'] = port
     nmap_args['verbose'] = verbose
+    nmap_args['output'] = '.log.tmp'
 
     run_nmap(nmap_args)
-    check_proxys()
+    check_proxys(op.url, nmap_args['output'])
--- scapegoat_test.py
+++ scapegoat_test.py
@@ -1,3 +1,4 @@
+import os
 import socket
 from urllib2 import urlopen
 
@@ -6,16 +7,74 @@ import unittest
 import scapegoat
 
 class ScapeGoatUnitTest(unittest.TestCase):
+    '''
+    def __init__(self):
+        self.test_url = 'http://chioka.in/wp-content/themes/girl/style.css'
+        self.test_log = '.log.tmp'
+    '''
+    test_url = 'http://chioka.in/wp-content/themes/girl/style.css'
+    test_log = '.log.tmp'
+
+    def test_run_nmap(self):
+        nmap_args = {}
+        nmap_args['ip'] = '127.0.0.1'
+        nmap_args['-p'] = '80,8080'
+        nmap_args['verbose'] = False
+        nmap_args['output'] = self.test_log
+
+        scapegoat.run_nmap(nmap_args)
+
+        log_data = [ i.rstrip() for i in open(self.test_log, 'r').readlines() ]
+
+        log_header = log_data[0]
+        self.assertTrue( 'Nmap' in log_header )
+        self.assertTrue( nmap_args['ip'] in log_header )
+        self.assertTrue( nmap_args['-p'] in log_header )
+        self.assertTrue( nmap_args['output'] in log_header )
+
+        run_success = False
+
+        for line in log_data[1:-1]:
+            if nmap_args['ip'] in line and ( 'open' in line or 'closed' in line or 'filtered' in line ) :
+                run_success = True
+
+        # Check if there is at least one avaliable proxy
+        self.assertTrue(run_success)
+
+        # Clean up
+        os.remove(self.test_log)
+
     def test_check_proxys(self):
-        pass
+        test_log = self.test_log
+        test_url = self.test_url
+
+        log_data = \
+'''
+# Nmap 4.68 scan initiated Sun Sep 21 20:25:44 2008 as: nmap -p 22,23,80 -oG log2.txt 203.168.164.68 
+Host: 127.0.0.1 (lssh) Ports: 22/open/tcp//ssh///, 23/filtered/tcp//telnet///, 80/closed/tcp//http///, 8080/open/tcp/http///
+# Nmap done at Sun Sep 21 20:25:47 2008 -- 1 IP address (1 host up) scanned in 3.370 seconds
+'''
+
+        expected_proxys = [{'ip':'127.0.0.1', 'port':'8080', 'user':'', 'pass':''}]
+
+        log_file = open(test_log, 'w')
+        log_file.write(log_data)
+        log_file.close()
+
+        proxys = scapegoat.check_proxys(test_url, test_log)
+        self.assertTrue(proxys, expected_proxys)
+
+        # Check for unremoved log file
+        for filename in os.listdir('.'):
+            self.assertTrue( test_log != filename )
+                
 
     def test_check_proxy(self):
-        control_url = 'http://chioka.in/wp-content/themes/girl/style.css'
-
-        url_sock = urlopen(control_url)
+        test_url = self.test_url
+        url_sock = urlopen(test_url)
         url_data = url_sock.read()
         
-        self.assertTrue( scapegoat.check_proxy('127.0.0.1','8080', url_data) )
+        self.assertTrue( scapegoat.check_proxy('127.0.0.1','8080', test_url, url_data) )
 
 
 socket.setdefaulttimeout(10)
